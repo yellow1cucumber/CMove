1bd3d013726a419adc0aa04779e4d02d20d04f06
sadovskiy1n@protonmail.com <sadovskiy1n@protonmail.com>
2024-08-01 19:13:39 +0500

    FEAT: connect qfiledialog open and path repository

--- CMove.cpp
+++ CMove.cpp
@@ -4,7 +4,42 @@ CMove::CMove(QWidget *parent)
     : QMainWindow(parent)
 {
     this->ui.setupUi(this);
+
+    this->connect(this->ui.SelectSourcePathButton, &QPushButton::clicked, 
+                  this, &CMove::OpenFileSourceDialog);
+    this->connect(this->ui.SelectDestinationPathButton, &QPushButton::clicked,
+                  this, &CMove::OpenFileDestinationDialog);
 }
 
 CMove::~CMove()
 {}
+
+QString CMove::GetDirByFileDialog()
+{
+    QString dir = QFileDialog::getExistingDirectory(
+        this,
+        tr("Source directory"),
+        "C:",
+        QFileDialog::DontResolveSymlinks
+        );
+    return dir;
+}
+
+
+#pragma region Slots
+void CMove::OpenFileSourceDialog() {
+    QString dir = this->GetDirByFileDialog();
+    emit onSourcePathSelected(dir);
+}
+void CMove::OpenFileDestinationDialog() {
+    QString dir = this->GetDirByFileDialog();
+    emit onDestinationPathSelected(dir);
+}
+
+void CMove::SetSourcePath(const QString& path) {
+    this->pathRepository.SetSourcePath(path);
+}
+void CMove::SetDestinationPath(const QString& path) {
+    this->pathRepository.SetDestinationPath(path);
+}
+#pragma endregion
--- CMove.h
+++ CMove.h
@@ -3,6 +3,10 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_CMove.h"
 
+#include "qdir.h"
+#include "qfiledialog.h"
+#include "qpushbutton.h"
+
 #include "PathRepository.h"
 
 class CMove : public QMainWindow
@@ -13,8 +17,21 @@ public:
     CMove(QWidget *parent = nullptr);
     ~CMove();
 
+    QString GetDirByFileDialog();
+
+signals:
+    void onDestinationPathSelected(const QString& path);
+    void onSourcePathSelected(const QString& path);
+
+public slots:
+    void OpenFileSourceDialog();
+    void OpenFileDestinationDialog();
+
+    void SetSourcePath(const QString& path);
+    void SetDestinationPath(const QString& path);
+
 private:
     Ui::CMoveClass ui;
 
-    PathRepository pathRepository;
+    PathRepository pathRepository{ this };
 };
